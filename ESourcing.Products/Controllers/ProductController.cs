f668ded5ccfe40b6db428df35d313b72c1ea74e8
Admin <Admin@LFPC1>
2022-01-03 15:59:14 +0300

    Proje dosyası ekle.

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController :ControllerBase
+    {
+    }
+}
