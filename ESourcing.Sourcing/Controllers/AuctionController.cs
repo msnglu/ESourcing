4159a227d7f9c0d97f4f842c449f3e2c1c40079a
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-01-05 18:03:33 +0300

    next step

---
+++ ESourcing.Sourcing/Controllers/AuctionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Sourcing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+
+    }
+}
