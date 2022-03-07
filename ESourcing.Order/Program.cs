976fe25d3705153ba658fd303333828d474c0537
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-03-07 18:04:01 +0300

    update program cs

--- ESourcing.Order/Program.cs
+++ ESourcing.Order/Program.cs
@@ -13,9 +13,7 @@ app.MigrateDatabase();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
 }
 
 app.Run();
