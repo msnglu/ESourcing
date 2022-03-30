ff1196de90ba8a095715d75863670092a84133a6
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-03-30 17:11:03 +0300

    consumer error handling

--- ESourcing.Order/Program.cs
+++ ESourcing.Order/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+builder.Host.UseDefaultServiceProvider(s => s.ValidateScopes =false);
 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 #region EventBus RabbitMQ
